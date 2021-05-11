4b5a42bcede27a34ce011bd9ca0b2548147f9fb1
AnthonyLedesma <anthonymledesma@gmail.com>
2021-05-11 13:29:59 -0700

    change default gutter value to small

--- src/blocks/gallery-offset/index.js
+++ src/blocks/gallery-offset/index.js
@@ -47,12 +47,13 @@ const settings = {
 	supports: {
 		align: [ 'wide', 'full' ],
 		gutter: {
-			default: 'medium',
+			default: 'small',
 		},
 	},
 	example: {
 		attributes: {
-			gutter: 'medium',
+			gutter: 'custom',
+			gutterCustom: 0,
 			images: [
 				{ index: 0, url: 'https://s.w.org/images/core/5.3/Sediment_off_the_Yucatan_Peninsula.jpg' },
 				{ index: 1, url: 'https://s.w.org/images/core/5.3/Windbuchencom.jpg' },
